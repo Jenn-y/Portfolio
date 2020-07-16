b8fc6aafc6ee7f2a885c1e4210bc98bd0af61abf
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-07-16 15:08:30 +0200

    integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,10 @@
 class Portfolio < ApplicationRecord
+  has_many :technologies
   include Placeholder
 
   validates_presence_of :title, :body, :main_image, :thumb_image
 
   scope :angular, -> { where(subtitle: 'Angular') }
-
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 
   after_initialize :set_defaults
