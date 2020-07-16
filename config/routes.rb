d89db0b4ea064369db3cdf81ad6373ed0ae29a7b
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-07-16 14:12:25 +0200

    implemented custom scopes for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolios#angular'
+  get 'ruby-on-rails-items', to: 'portfolios#ruby_on_rails'
   
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
