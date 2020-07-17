8a29820ec196cccf2e3fed6a52bd25c88ce38ec5
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-07-17 21:10:07 +0200

    added navbar, default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
-
-  def current_user
-    super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
-  end
+  include CurrentUserConcern
+  include DefaultPageContent
 end
