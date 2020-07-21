ca6a61ff58f81b1296616caf045109f8a3e51b85
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-07-21 20:18:48 +0200

    integrated cacoon for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,7 +25,6 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -41,7 +40,6 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    3.times { @portfolio_item.technologies.build }
   end
 
   def update
@@ -71,7 +69,7 @@ class PortfoliosController < ApplicationController
                                       :thumb_image,
                                       :main_image,
                                       :body, 
-                                      technologies_attributes: [:name]
+                                      technologies_attributes: [:id, :name, :_destroy]
                                       )
   end
 
