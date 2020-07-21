60fa9293ff62790684f3cc1c0faeb133165220b5
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-07-21 16:21:32 +0200

    updated file upload forms and refactored image placeholder

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,21 +3,14 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                  reject_if: lambda { |attr| attr['name'].blank? }
 
-  include Placeholder
 
-  validates_presence_of :title, :body, :main_image, :thumb_image
-  after_initialize :set_defaults
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
 
   scope :angular, -> { where(subtitle: 'Angular') }
   scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 
   def self.by_position
     order("position ASC")
