c2462933e56ed998cb595e4f6453340d7ba29681
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-07-21 15:30:44 +0200

    implemented initial file uploading system for portfolio items via carrierwave

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -68,6 +68,8 @@ class PortfoliosController < ApplicationController
   def portfolio_params
     params.require(:portfolio).permit(:title, 
                                       :subtitle, 
+                                      :thumb_image,
+                                      :main_image,
                                       :body, 
                                       technologies_attributes: [:name]
                                       )
