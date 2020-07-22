e851fa3e91c52f2cc4dab86160c4fa07bcf4b94a
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-07-22 15:08:32 +0200

    refactored form into partial for comments and added validation to comment model

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require('cable')
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
-require('action_cable')
 
 // jquery
 import $ from 'jquery';
